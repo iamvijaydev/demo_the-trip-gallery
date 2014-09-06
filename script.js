cfb1f2520c568468a673d8c9daaf6d27ec5cacc3
Vijay Dev <vijaydev.in@gmail.com>
2014-09-06 15:23:53 +0530

    pushing files

---
+++ script.js
@@ -0,0 +1,46 @@
+
+// Run along now, this is a private party
+(function(document, window) {
+
+	// DOM nodes and objects instances
+	var $thelist, $thelistChilds, $wrapper, myScroll, loaded;
+
+	// numerical variables
+	var i = 0, left = 0;
+
+	// exec after DOM loaded
+	loaded = function() {
+
+		// assign lefts to each child
+		$thelist = document.getElementById('thelist');
+		$thelistChilds = $thelist.children;
+		for (i = 0; i < $thelistChilds.length; i++) {
+			left += 500;
+			$thelistChilds[i].style.left = left + 'px';
+		};
+
+		// setup iScroll
+		$wrapper = document.getElementById('wrapper');
+		myScroll = new IScroll($wrapper, {
+			'scrollbars': 'custom',
+			'mouseWheel': true,
+			'bounce': false,
+			'interactiveScrollbars': true,
+			'eventPassthrough': true,
+			'scrollX': true,
+			'scrollY': false
+		});
+	}
+
+	// if its a touch screen
+	if ( 'ontouchstart' in window ) {
+
+		// avoid and browser screen bounce
+		document.addEventListener('touchmove', function (e) {
+			e.preventDefault();
+		}, false);
+	};
+	
+	// setup DOM ready
+	document.addEventListener('DOMContentLoaded', loaded, false);
+})(document, window);
